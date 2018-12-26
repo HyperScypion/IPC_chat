ba9cdd1d9b2e1417f6aae0b1c88c6255b7f4846a
Tomasz Derek <t.derek@g.loiv.torun.pl>
2018-12-26 12:35:20 +0100

    Create sysv_queue.h

---
+++ sysv_queue.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#define MAXLINE 1024
+
+struct my_msgbuf {
+    long mtype;
+    char mtext[MAXLINE];
+    char nickname[MAXLINE];
+    long pid;
+    int option;
+};
